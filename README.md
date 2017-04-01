31b1051863ac3c2d45992e6328c1196294f401e3
Gabriel Morales <grmorale@gmail.com>
2017-03-31 20:30:13 -0700

    Improve Computer's Guessing Logic

--- README.md
+++ README.md
@@ -2,8 +2,6 @@ The classic Mastermind game.
 
 Player vs Computer 
 
-The computer chooses four random colors. The player must guess these colors within 12 turns. 
-
-(In mastermind_two.rb, the player can reverse roles with the computer)
+The computer chooses four random colors. The player must guess these colors within 12 turns. The player can reverse roles with the computer.
 
 After each guess, the player is given feedback in the form of a white or black response. "White" means a correct color, but in the wrong position. "Black" means a correct color in the right position.
--- mastermind.rb
+++ mastermind.rb
@@ -1,29 +1,95 @@
 class Mastermind
-  attr_accessor :feedback, :counter
-  attr_reader :colors, :answer
   def initialize()
     @colors=["GREEN","BLUE","RED","YELLOW"]
     @answer=[]
     @feedback=[]
-    @counter=0
-    4.times do
-      @answer<<@colors.sample
-    end
-    
+    @counter=0     
   end
 
-  def game_on()    
+  def selector()
+    puts "Choose CREATE or SOLVE"
+    choice=gets.chomp
+    choice.upcase!
+    puts "You chose #{choice}"
+    while (choice!="CREATE" && choice!="SOLVE") do
+      puts "Choose CREATE or SOLVE"
+      choice=gets.chomp.upcase!
+    end
+
+    if choice=="CREATE"
+      @guess=[" "," "," "," "]
+      puts "#{@colors}"
+      4.times do
+        puts "Enter the ARRAY VALUE for a color: GREEN[0], BLUE[1], RED[2], or YELLOW[3]"
+        num=gets.chomp
+        @answer<<@colors[num.to_i]
+      end
+      puts "#{@answer}"
+      comp_plays()
+    elsif choice=="SOLVE"
+      4.times do
+        @answer<<@colors.sample
+      end
+      user_plays()
+    end
+  end
+
+  def user_plays()      
     while @counter<12 do 
     puts "Turn #{@counter}"
     puts "Choose from #{@colors}"
     guess_arr=user_input()
     compare(guess_arr)
     puts ""
-    feed_back(guess_arr)
+    feedback(guess_arr)
     @counter+=1
     end
     puts "Game over"
     puts "Answer was #{@answer}"
+  end
+
+  def comp_plays()   
+    puts "Computer chooses from #{@colors}"    
+    while @counter<12 do 
+    puts "Turn #{@counter}"   
+    comp_input()
+    puts "Comp's guess: #{@guess}"
+    compare(@guess)  
+    @counter+=1
+    puts ""
+    end
+    puts "Game over"
+    puts "Answer was #{@answer}"
+  end
+
+  def comp_input()
+    j=0
+    i=0
+    newguess=[]
+    while j<4 && @counter>0 do
+      if @answer[i]!=@guess[i]
+        if @answer.include? (@guess[i])
+          newguess<<@guess[i]
+        end
+      end
+      j+=1
+    end
+      
+    while i<4 do
+      if @answer[i]!=@guess[i]
+        @guess[i]=@colors.sample
+      end
+      i+=1
+      if i==4
+        newguess.each do |x|
+        puts "Must include #{x}"
+        if @guess.include?(x)==false
+          i=0
+        end
+        end
+      end
+    end
+
   end
 
   def user_input()
@@ -38,18 +104,16 @@ class Mastermind
 
   def compare(arr)
     if @answer==arr
-      puts "Player wins!"
+      puts "Victory!"
       @counter=12
     end    
   end
-  #consider using numeric feedback ie "2 choices wrong, 1 right"
-  def feed_back(arr)
+
+  def feedback(arr)
     @feedback=[]
     i=0
-    g=0
     j=0   
     copy=@answer.clone
-    #puts "Copy is #{copy}"
     while i<4 do
       if copy[i]==arr[i]
         @feedback<<"Black"
@@ -66,10 +130,10 @@ class Mastermind
       end
       j+=1
     end
-    puts "#{@feedback}"
+    puts "Feedback: #{@feedback}"
   end
 
 end
 
 game=Mastermind.new
-game.game_on()
+game.selector()
